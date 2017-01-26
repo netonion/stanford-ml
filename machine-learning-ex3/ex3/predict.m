50358b9d6b3272b6fea2ba089c20b20f038a57c6
Sam Guo <netonion@hotmail.com>
2017-01-25 23:58:07 -0500

    Complete part 2 prediction with Neural Network

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,6 +21,9 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a2 = sigmoid([ones(m, 1) X] * Theta1');
+a3 = sigmoid([ones(m, 1) a2] * Theta2');
+[~, p] = max(a3, [], 2);
 
 
 
