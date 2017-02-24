990107af4dd16d41445cd6cdcb4303d0323a09d1
Sam Guo <netonion@hotmail.com>
2017-02-24 08:13:58 -0500

    Complete ex6 part 1 SVM

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,6 +23,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+range = [0.01 0.03 0.1 0.3 1 3 10 30];
+best = Inf;
+for _C = range
+  for _sigma = range
+    model= svmTrain(X, y, _C, @(x1, x2) gaussianKernel(x1, x2, _sigma));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+    if error < best
+      best = error;
+      C = _C;
+      sigma = _sigma;
+    end
+  end
+end
 
 
 
